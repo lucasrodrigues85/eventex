73418ea42ddc58b058122da420a8ba31dbd8cb0c
Lucas Rodrigues <lucas@scienceweb.com.br>
2022-08-13 12:11:09 -0300

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Lucas Rodrigues',
+            cpf='12345678901',
+            email='lucas@scienceweb.com.br',
+            phone='12-90000-0000'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr"""
+        self.assertIsInstance(self.obj.created_at, datetime)
