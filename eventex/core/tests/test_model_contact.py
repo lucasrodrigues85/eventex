45a5c33f942d4922ba2506f2e1ab3403ca24ceb0
Lucas Rodrigues <lucas@scienceweb.com.br>
2022-09-13 10:32:54 -0300

    Managers

--- eventex/core/tests/test_model_contact.py
+++ eventex/core/tests/test_model_contact.py
@@ -16,11 +16,11 @@ class ContactModelTest(TestCase):
                                          value='henrique@bastos.net')
 
         self.assertTrue(Contact.objects.exists())
-    
+
     def test_phone(self):
         contact = Contact.objects.create(speaker=self.speaker, kind=Contact.PHONE,
                                          value='21-996186180')
-        
+
         self.assertTrue(Contact.objects.exists())
 
     def test_choice(self):
@@ -29,5 +29,28 @@ class ContactModelTest(TestCase):
         self.assertRaises(ValidationError, contact.full_clean)
 
     def test_str(self):
-        contact = Contact(speaker=self.speaker, kind=Contact.EMAIL, value='henrique@bastos.net')
+        contact = Contact(speaker=self.speaker,
+                          kind=Contact.EMAIL, value='henrique@bastos.net')
         self.assertEqual('henrique@bastos.net', str(contact))
+
+
+class ContactManagerTest(TestCase):
+    def setUp(self):
+        s = Speaker.objects.create(
+            name='Henrique Bastos',
+            slug='henrique-bastos',
+            photo='http://hbn.link/hb-pic'
+        )
+
+        s.contact_set.create(kind=Contact.EMAIL, value='henrique@bastos.net')
+        s.contact_set.create(kind=Contact.PHONE, value='21-996186180')
+
+    def test_emails(self):
+        qs = Contact.objects.emails()
+        expected = ['henrique@bastos.net']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
+
+    def test_phones(self):
+        qs = Contact.objects.phones()
+        expected = ['21-996186180']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
